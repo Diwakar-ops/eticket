4af99db3a89038920c576296114074065e67f2f4
Diwakar-ops <65555408+Diwakar-ops@users.noreply.github.com>
2021-11-07 09:41:40 -0800

    Update urls.py

--- eticket/urls.py
+++ eticket/urls.py
@@ -23,6 +23,5 @@ from . import settings
 urlpatterns = [
     path('',include('calc.urls')),
     path('admin/', admin.site.urls),
-url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
